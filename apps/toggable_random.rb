2ec4e53bafaa376f3e67c2e0d75c301f8d304321
Sam Aaron <samaaron@gmail.com>
2008-06-14 14:46:11 +0200

    using threads it's possible to start combining functionality in interesting ways. Consider this version of random which allows you to toggle the lights as well...

---
+++ apps/toggable_random.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby -wKU
+
+require '../lib/monome'
+
+class ToggableRandom
+  def initialize
+    @monome = Monome::Monome.new
+    @monome.listeners << Monome::Listeners::Toggle.new <<
+                         Monome::Listeners::CornerToggles.new
+  end
+  
+  def start
+    monome_thread = Thread.new { @monome.start }
+    random_thread = Thread.new do
+      10000000.times do
+        sleep(rand(0.5))
+        toggle_random_led
+      end
+    end
+    monome_thread.join
+    random_thread.join
+  end
+  
+  def toggle_random_led
+    x = rand(@monome.max_x + 1)
+    y = rand(@monome.max_y + 1)
+    @monome.toggle_led(x,y)
+  end
+end
+
+ToggableRandom.new.start
