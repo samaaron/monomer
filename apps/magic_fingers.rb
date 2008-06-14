ac050c8be3130fdcf2ec26bb7f888514a1feb9e3
Sam Aaron <samaaron@gmail.com>
2008-06-14 23:48:26 +0200

    interesting variant on the toggle theme. pressing a key toggles its neighbouring keys.

---
+++ apps/magic_fingers.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby -wKU
+
+require '../lib/monome'
+
+class MagicFingers
+  def initialize
+    @monome = Monome::Monome.new
+    @monome.listeners << self << Monome::Listeners::CornerToggles.new
+  end
+  
+  def button_pressed(x,y)
+    draw_toggle_square(x,y,1)
+  end
+  
+  def draw_toggle_square(x,y,size)
+    @monome.toggle_led(x + size ,y + size)
+    @monome.toggle_led(x + size ,y       )
+    @monome.toggle_led(x        ,y + size)
+    @monome.toggle_led(x - size ,y - size)
+    @monome.toggle_led(x - size ,y       )
+    @monome.toggle_led(x        ,y - size)
+    @monome.toggle_led(x - size ,y + size)
+    @monome.toggle_led(x + size ,y - size)
+  end
+  
+  def start
+    @monome.start
+  end
+end
+
+MagicFingers.new.start 
+
+
