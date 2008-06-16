d07974afe009100ae947837fba7fc061f941cbfb
Sam Aaron <samaaron@gmail.com>
2008-06-17 00:25:20 +0200

    no longer require defunct listeners

--- lib/monomer.rb
+++ lib/monomer.rb
@@ -9,11 +9,6 @@ require 'monome/state'
 require 'monome/message'
 require 'monome/listener'
 
-#require listeners
-require 'listeners/corner_toggles'
-require 'listeners/rectangles'
-require 'listeners/toggle'
-
 #require crazy shit
 require '_why/metaid'
 
